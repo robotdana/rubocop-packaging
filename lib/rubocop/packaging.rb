5de0e91d126965ad67b68ccce9ef13b7caea49b2
Dana Sherson <robot@dana.sh>
2020-06-07 17:19:32 +1000

    Initial commit

---
+++ lib/rubocop/packaging.rb
@@ -0,0 +1,8 @@
+require "rubocop/packaging/version"
+
+module Rubocop
+  module Packaging
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/packaging/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Packaging
+    VERSION = "0.1.0"
+  end
+end
